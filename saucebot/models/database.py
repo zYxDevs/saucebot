ffc6d0eb3b5476102ab441c8b98b4cb87f6cc16f
Makoto <FujiMakoto@users.noreply.github.com>
2020-06-06 04:16:10 -0400

    Database models

---
+++ saucebot/models/database.py
@@ -0,0 +1,26 @@
+import discord
+from pony.orm import *
+
+from saucebot.log import log
+
+db = Database()
+db.bind(provider='sqlite', filename='database.sqlite', create_db=True)
+
+
+# noinspection PyMethodParameters
+class Servers(db.Entity):
+    server_id = Required(int, size=64, unique=True)
+    api_key = Optional(str, 40)
+
+    @db_session
+    def register(guild: discord.Guild, api_key: str):
+        # Delete any existing entry for this server
+        server = Servers.get(server_id=guild.id)
+        if server:
+            server.delete()
+
+        Servers(server_id=guild.id, api_key=api_key)
+        log.info(f'Registering API key for server {guild.name} ({guild.id})')
+
+
+db.generate_mapping(create_tables=True)
