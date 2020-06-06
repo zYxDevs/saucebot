dfa3eb7dda961ba8baebdc9a7efcb7e280cdb399
Makoto <FujiMakoto@users.noreply.github.com>
2020-06-06 02:39:45 -0400

    Helper functions

---
+++ saucebot/helpers.py
@@ -0,0 +1,37 @@
+import re
+
+import discord
+
+from saucebot.bot import bot
+from saucebot.log import log
+
+# https://daringfireball.net/2010/07/improved_regex_for_matching_urls
+URL_REGEX = re.compile(r"(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))")
+
+
+def validate_url(url: str) -> bool:
+    """
+    Validates URL's for security
+    Args:
+        url (str): The URL to validate
+
+    Returns:
+        bool
+    """
+    if not URL_REGEX.match(url):
+        log.debug("URL failed to match: " + url)
+        return False
+
+    return True
+
+
+def basic_embed(**kwargs) -> discord.Embed:
+    """
+    Generates a boilerplate embed with the bots avatar as the thumbnail
+    Returns:
+        discord.Embed
+    """
+    embed = discord.Embed(**kwargs)
+    embed.set_thumbnail(url=bot.user.avatar_url)
+
+    return embed
