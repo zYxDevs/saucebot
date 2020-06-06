f0f129ff12a1d42bed9050c111cfb3677a243a4f
Makoto <535086-FujiMakoto@users.noreply.gitlab.com>
2020-06-06 01:03:38 -0400

    Bot info command

--- saucebot/cogs/misc.py
+++ saucebot/cogs/misc.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import discord
 from discord.ext import commands
 
+from saucebot.bot import bot
 from saucebot.lang import lang
 
 
@@ -24,13 +25,25 @@ class Misc(commands.Cog):
         response_time = round((now - ctx.message.created_at.timestamp()) * 1000, 1)
 
         embed = discord.Embed()
-        embed.description = lang('Misc', 'ping_response', {'command': response_time, 'message': 'Pending...'})
+        embed.description = lang('Misc', 'ping_response', {'server': response_time, 'message': 'Pending...'})
 
         # Time between when we sent the message and when it was registered by Discord
         now = datetime.utcnow().timestamp()
         message = await ctx.send(embed=embed)
-        response_time = round((now - ctx.message.created_at.timestamp()) * 1000, 1)
-        message_delay = round((now - message.created_at.timestamp()) * 1000, 1)
+        message_delay = round((datetime.utcnow().timestamp() - now) * 1000, 1)
 
-        embed.description = lang('Misc', 'ping_response', {'command': response_time, 'message': message_delay})
+        embed.description = lang('Misc', 'ping_response', {'server': response_time, 'message': message_delay})
         await message.edit(embed=embed)
+
+    @commands.command(alias=['support', 'patreon'])
+    async def info(self, ctx: commands.context.Context):
+        """
+        Learn more about the SauceBot project and how to contribute!
+        """
+        embed = discord.Embed()
+        embed.set_thumbnail(url=bot.user.avatar_url)
+        embed.title = lang('Misc', 'info_title')
+        embed.url = 'https://github.com/FujiMakoto/saucebot'
+        embed.description = lang('Misc', 'info_desc')
+
+        await ctx.send(embed=embed)
