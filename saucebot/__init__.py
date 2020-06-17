907fb9dea0e2d82b6c95ca3ac88aba1e5674d5c7
Makoto <FujiMakoto@users.noreply.github.com>
2020-06-17 06:14:56 -0400

    Copyright metadata

---
+++ saucebot/__init__.py
@@ -0,0 +1,7 @@
+__author__      = 'FujiMakoto'
+__copyright__   = 'Copyright 2020, Taiga.sh Development'
+__credits__     = ['FujiMakoto']
+__license__     = 'gpl-3.0'
+__maintainer__  = 'FujiMakoto'
+__email__       = 'makoto+github@taiga.sh'
+__status__      = 'Development'
