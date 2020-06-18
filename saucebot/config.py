60a72e4d79ba290e0962162cbb273b265547efc3
Makoto <FujiMakoto@users.noreply.github.com>
2020-06-18 06:58:31 -0400

    Updated default configuration

--- saucebot/config.py
+++ saucebot/config.py
@@ -3,3 +3,6 @@ from configparser import ConfigParser
 
 config = ConfigParser()
 config.read(['config.default.ini', 'config.ini'])
+
+server_api_limit = config.get('SauceNao', 'server_api_limit', fallback=None)
+member_api_limit = config.get('SauceNao', 'member_api_limit', fallback=None)
