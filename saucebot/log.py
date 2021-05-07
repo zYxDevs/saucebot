668543f82736a0fdc75f0e698b46a5460b1b7d33
Makoto <makoto+github@taiga.sh>
2021-05-06 22:32:00 -0400

    sentry.io integration

--- saucebot/log.py
+++ saucebot/log.py
@@ -1,6 +1,8 @@
 # Set up logging
 import logging
 
+import sentry_sdk
+
 from saucebot.config import config
 
 logLevel = getattr(logging, str(config.get('Bot', 'log_level', fallback='ERROR')).upper())
@@ -20,3 +22,7 @@ ch.setFormatter(logFormat)
 
 log.addHandler(ch)
 query_log.addHandler(ch)
+
+# Unless you're running your own custom fork of saucebot, you probably don't need this.
+if config.has_option('Bot', 'sentry_logging') and config.getboolean('System', 'sentry_logging'):
+    sentry_sdk.init(config.get('Bot', 'sentry_dsn'), traces_sample_rate=0.25)
