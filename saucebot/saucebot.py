4a6de534e16627aab0deb200795495bc10a06788
Makoto <makoto+github@taiga.sh>
2021-05-06 21:24:36 -0400

    Suppress "command not found" errors

--- saucebot/saucebot.py
+++ saucebot/saucebot.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext import commands
 
 from saucebot.bot import bot
 from saucebot.cogs.admin import Admin
@@ -6,10 +7,25 @@ from saucebot.cogs.misc import Misc
 from saucebot.cogs.sauce import Sauce
 from saucebot.log import log
 
-
 bot.add_cog(Sauce())
 bot.add_cog(Misc())
 bot.add_cog(Admin())
+
+
+@bot.event
+async def on_command_error(ctx: commands.Context, error: Exception):
+    # If command has local error handler, return
+    if hasattr(ctx.command, 'on_error'):
+        return
+
+    # Get the original exception
+    error = getattr(error, 'original', error)
+
+    # Stop spamming our console everytime someone uses another bots command
+    if isinstance(error, commands.CommandNotFound):
+        return
+
+    raise error
 
 
 @bot.event
